a15f8fc044d50500e04f0a0446e09943b17d8e16
Niklas Laxström <niklas.laxstrom@gmail.com>
2013-09-25 17:33:15 -0700

    Implement first scenario for main page
    Bunnies say yay!
    
    Change-Id: I5a1be959618f740c3674150a770912ce02f7beb2

--- tests/browser/features/support/pages/main_page.rb
+++ tests/browser/features/support/pages/main_page.rb
@@ -3,4 +3,7 @@ class MainPage
 
   include URL
   page_url URL.url('Special:MainPage')
+
+	div(:number_of_translators, id: 'twnmp-s-translators')
+	div(:number_of_projects, id: 'twnmp-s-projects')
 end

6c3881fe7bf1ad185ca4fcdac9b662793823f439
Željko Filipin <zeljko.filipin@gmail.com>
2017-09-29 18:50:53 +0200

    Deleted all Ruby code
    The only Ruby code were broken Selenium tests.
    
    Bug: T164479
    Depends-On: Iec1100b8c3911254542264b89f6e4a0a8b87c4c8
    Change-Id: Ibcb7fdd21a6f23476303a1b463ae368767127ffb

--- tests/browser/features/support/pages/preferences_page.rb
+++
@@ -1,8 +0,0 @@
-class PreferencesPage
-  include PageObject
-
-  page_url 'Special:Preferences'
-
-  select(:set_interface_language, id: 'mw-input-wplanguage')
-  button(:save, id: 'prefcontrol')
-end

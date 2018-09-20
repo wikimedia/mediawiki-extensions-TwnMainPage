5e669397e42cafda4737971bea4ae5c5adb35e1f
Umherirrender <umherirrender_de.wp@web.de>
2018-09-20 22:12:35 +0200

    Use short array syntax in i18n alias file
    Change-Id: I4dfb61bbcced7c1f7fcade00ad536f4f3bbd65b9

--- MainPage.alias.php
+++ MainPage.alias.php
@@ -5,161 +5,160 @@
  * @file
  * @ingroup Extensions
  */
-// @codingStandardsIgnoreFile
 
-$specialPageAliases = array();
+$specialPageAliases = [];
 
 /** English (English) */
-$specialPageAliases['en'] = array(
-	'TwnMainPage' => array( 'MainPage', 'FirstSteps' ),
-);
+$specialPageAliases['en'] = [
+	'TwnMainPage' => [ 'MainPage', 'FirstSteps' ],
+];
 
 /** Arabic (العربية) */
-$specialPageAliases['ar'] = array(
-	'TwnMainPage' => array( 'الصفحة_الرئيسية' ),
-);
+$specialPageAliases['ar'] = [
+	'TwnMainPage' => [ 'الصفحة_الرئيسية' ],
+];
 
 /** Egyptian Arabic (مصرى) */
-$specialPageAliases['arz'] = array(
-	'TwnMainPage' => array( 'الصفحه_الرئيسيه' ),
-);
+$specialPageAliases['arz'] = [
+	'TwnMainPage' => [ 'الصفحه_الرئيسيه' ],
+];
 
 /** Asturian (asturianu) */
-$specialPageAliases['ast'] = array(
-	'TwnMainPage' => array( 'Portada' ),
-);
+$specialPageAliases['ast'] = [
+	'TwnMainPage' => [ 'Portada' ],
+];
 
 /** Western Balochi (بلوچی رخشانی) */
-$specialPageAliases['bgn'] = array(
-	'TwnMainPage' => array( 'سر_وّرق' ),
-);
+$specialPageAliases['bgn'] = [
+	'TwnMainPage' => [ 'سر_وّرق' ],
+];
 
 /** Min Dong Chinese (Mìng-dĕ̤ng-ngṳ̄) */
-$specialPageAliases['cdo'] = array(
-	'TwnMainPage' => array( '頭頁' ),
-);
+$specialPageAliases['cdo'] = [
+	'TwnMainPage' => [ '頭頁' ],
+];
 
 /** German (Deutsch) */
-$specialPageAliases['de'] = array(
-	'TwnMainPage' => array( 'Hauptseite' ),
-);
+$specialPageAliases['de'] = [
+	'TwnMainPage' => [ 'Hauptseite' ],
+];
 
 /** Zazaki (Zazaki) */
-$specialPageAliases['diq'] = array(
-	'TwnMainPage' => array( 'PelaSeri' ),
-);
+$specialPageAliases['diq'] = [
+	'TwnMainPage' => [ 'PelaSeri' ],
+];
 
 /** Greek (Ελληνικά) */
-$specialPageAliases['el'] = array(
-	'TwnMainPage' => array( 'ΚύριαΣελίδα' ),
-);
+$specialPageAliases['el'] = [
+	'TwnMainPage' => [ 'ΚύριαΣελίδα' ],
+];
 
 /** Persian (فارسی) */
-$specialPageAliases['fa'] = array(
-	'TwnMainPage' => array( 'صفحهٔ_اصلی' ),
-);
+$specialPageAliases['fa'] = [
+	'TwnMainPage' => [ 'صفحهٔ_اصلی' ],
+];
 
 /** Galician (galego) */
-$specialPageAliases['gl'] = array(
-	'TwnMainPage' => array( 'Portada' ),
-);
+$specialPageAliases['gl'] = [
+	'TwnMainPage' => [ 'Portada' ],
+];
 
 /** Hebrew (עברית) */
-$specialPageAliases['he'] = array(
-	'TwnMainPage' => array( 'צעדים_ראשונים_בתרגום' ),
-);
+$specialPageAliases['he'] = [
+	'TwnMainPage' => [ 'צעדים_ראשונים_בתרגום' ],
+];
 
 /** Upper Sorbian (hornjoserbsce) */
-$specialPageAliases['hsb'] = array(
-	'TwnMainPage' => array( 'Hłowna_strona' ),
-);
+$specialPageAliases['hsb'] = [
+	'TwnMainPage' => [ 'Hłowna_strona' ],
+];
 
 /** Italian (italiano) */
-$specialPageAliases['it'] = array(
-	'TwnMainPage' => array( 'PaginaPrincipale' ),
-);
+$specialPageAliases['it'] = [
+	'TwnMainPage' => [ 'PaginaPrincipale' ],
+];
 
 /** Japanese (日本語) */
-$specialPageAliases['ja'] = array(
-	'TwnMainPage' => array( 'メインページ' ),
-);
+$specialPageAliases['ja'] = [
+	'TwnMainPage' => [ 'メインページ' ],
+];
 
 /** Georgian (ქართული) */
-$specialPageAliases['ka'] = array(
-	'TwnMainPage' => array( 'მთავარი_გვერდი' ),
-);
+$specialPageAliases['ka'] = [
+	'TwnMainPage' => [ 'მთავარი_გვერდი' ],
+];
 
 /** Korean (한국어) */
-$specialPageAliases['ko'] = array(
-	'TwnMainPage' => array( '대문' ),
-);
+$specialPageAliases['ko'] = [
+	'TwnMainPage' => [ '대문' ],
+];
 
 /** Kurdish (Latin script) (Kurdî (latînî)‎) */
-$specialPageAliases['ku-latn'] = array(
-	'TwnMainPage' => array( 'Destpêk' ),
-);
+$specialPageAliases['ku-latn'] = [
+	'TwnMainPage' => [ 'Destpêk' ],
+];
 
 /** Luxembourgish (Lëtzebuergesch) */
-$specialPageAliases['lb'] = array(
-	'TwnMainPage' => array( 'Haaptsäit' ),
-);
+$specialPageAliases['lb'] = [
+	'TwnMainPage' => [ 'Haaptsäit' ],
+];
 
 /** Literary Chinese (文言) */
-$specialPageAliases['lzh'] = array(
-	'TwnMainPage' => array( '卷首' ),
-);
+$specialPageAliases['lzh'] = [
+	'TwnMainPage' => [ '卷首' ],
+];
 
 /** Minangkabau (Baso Minangkabau) */
-$specialPageAliases['min'] = array(
-	'TwnMainPage' => array( 'Palanta' ),
-);
+$specialPageAliases['min'] = [
+	'TwnMainPage' => [ 'Palanta' ],
+];
 
 /** Macedonian (македонски) */
-$specialPageAliases['mk'] = array(
-	'TwnMainPage' => array( 'ГлавнаСтраница' ),
-);
+$specialPageAliases['mk'] = [
+	'TwnMainPage' => [ 'ГлавнаСтраница' ],
+];
 
 /** Malayalam (മലയാളം) */
-$specialPageAliases['ml'] = array(
-	'TwnMainPage' => array( 'പ്രധാനതാൾ' ),
-);
+$specialPageAliases['ml'] = [
+	'TwnMainPage' => [ 'പ്രധാനതാൾ' ],
+];
 
 /** Marathi (मराठी) */
-$specialPageAliases['mr'] = array(
-	'TwnMainPage' => array( 'मुखपृष्ठ' ),
-);
+$specialPageAliases['mr'] = [
+	'TwnMainPage' => [ 'मुखपृष्ठ' ],
+];
 
 /** Norwegian Bokmål (norsk bokmål) */
-$specialPageAliases['nb'] = array(
-	'TwnMainPage' => array( 'Hovedside' ),
-);
+$specialPageAliases['nb'] = [
+	'TwnMainPage' => [ 'Hovedside' ],
+];
 
 /** Dutch (Nederlands) */
-$specialPageAliases['nl'] = array(
-	'TwnMainPage' => array( 'Startpagina' ),
-);
+$specialPageAliases['nl'] = [
+	'TwnMainPage' => [ 'Startpagina' ],
+];
 
 /** Tuvinian (тыва дыл) */
-$specialPageAliases['tyv'] = array(
-	'TwnMainPage' => array( 'Кол_Арын' ),
-);
+$specialPageAliases['tyv'] = [
+	'TwnMainPage' => [ 'Кол_Арын' ],
+];
 
 /** Vietnamese (Tiếng Việt) */
-$specialPageAliases['vi'] = array(
-	'TwnMainPage' => array( 'Trang_Chính' ),
-);
+$specialPageAliases['vi'] = [
+	'TwnMainPage' => [ 'Trang_Chính' ],
+];
 
 /** Chinese (中文) */
-$specialPageAliases['zh'] = array(
-	'TwnMainPage' => array( '首页', '主页', '首頁', '主頁' ),
-);
+$specialPageAliases['zh'] = [
+	'TwnMainPage' => [ '首页', '主页', '首頁', '主頁' ],
+];
 
 /** Simplified Chinese (中文（简体）‎) */
-$specialPageAliases['zh-hans'] = array(
-	'TwnMainPage' => array( '首页', '第一步' ),
-);
+$specialPageAliases['zh-hans'] = [
+	'TwnMainPage' => [ '首页', '第一步' ],
+];
 
 /** Traditional Chinese (中文（繁體）‎) */
-$specialPageAliases['zh-hant'] = array(
-	'TwnMainPage' => array( '主頁' ),
-);
+$specialPageAliases['zh-hant'] = [
+	'TwnMainPage' => [ '主頁' ],
+];

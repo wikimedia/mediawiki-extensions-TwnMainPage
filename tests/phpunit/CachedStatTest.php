e143baf19187acaa27c61f3eca4f83006e748e24
Abijeet <abijeetpatro@gmail.com>
2019-08-26 23:01:55 +0530

    Fix failing build for TwnMainPage on HHVM
    Use a custom BagOStuff instead of a mocked BagOStuff to circumvent
    the lack of variadics support in HHVM. Without variadics support
    mocks cannot be created by PHPUnit.
    
    See: https://github.com/facebook/hhvm/issues/6954
    
    Bug: T231270
    Change-Id: Ia2161126e501d91b154eefd7bf18050b8de34cac

--- tests/phpunit/CachedStatTest.php
+++ tests/phpunit/CachedStatTest.php
@@ -7,14 +7,10 @@
 class CachedStatTest extends PHPUnit\Framework\TestCase {
 	use PHPUnit4And6Compat;
 
-	public static function updater() {
-		return 'test';
-	}
-
 	public function testDontAllowMiss() {
 		$value = 'test';
 
-		$updater = $this->getMock( 'NullUpdater' );
+		$updater = $this->getMock( NullUpdater::class );
 		$updater->expects( self::once() )
 			->method( 'calculate' )
 			->will( self::returnValue( $value ) );
@@ -27,19 +23,17 @@ class CachedStatTest extends PHPUnit\Framework\TestCase {
 			'update'
 		);
 
-		$stub = $this->getMock( 'EmptyBagOStuff' );
-		$stub->expects( self::once() )
-			->method( 'get' );
+		$bag = new TwnMainPageTestBagOfStuff( $value );
+		$cacher->setCache( $bag );
 
-		$stub->expects( self::once() )
-			->method( 'set' );
-
-		$cacher->setCache( $stub );
 		$this->assertEquals( $value, $cacher->get() );
+		$this->assertEquals( 1, $bag->getterCount() );
+		$this->assertEquals( 1, $bag->setterCount() );
 	}
 
 	public function testAllowMiss() {
-		$updater = $this->getMock( 'NullUpdater' );
+		$updater = $this->getMock( NullUpdater::class );
+
 		$updater->expects( self::never() )
 			->method( 'calculate' );
 
@@ -50,20 +44,46 @@ class CachedStatTest extends PHPUnit\Framework\TestCase {
 			[ [ $updater, 'calculate' ] ],
 			'allow miss'
 		);
-		$stub = $this->getMock( 'EmptyBagOStuff' );
-		$stub->expects( self::once() )
-			->method( 'get' );
 
-		$stub->expects( self::never() )
-			->method( 'set' );
+		$bag = new TwnMainPageTestBagOfStuff();
+		$cacher->setCache( $bag );
 
-		$cacher->setCache( $stub );
 		$this->assertEquals( null, $cacher->get() );
+		$this->assertEquals( 1, $bag->getterCount() );
+		$this->assertEquals( 0, $bag->setterCount() );
 	}
 }
 
 class NullUpdater {
 	public function calculate() {
 		return 'unreachable code';
+	}
+}
+
+class TwnMainPageTestBagOfStuff extends HashBagOStuff {
+	private $getCount = 0;
+	private $setCount = 0;
+	private $getValue;
+
+	public function __construct( $getValue = null ) {
+		$this->getValue = $getValue;
+	}
+
+	public function get( $key, $flags = 0 ) {
+		++$this->getCount;
+		return $this->getValue;
+	}
+
+	public function set( $key, $value, $exptime = 0, $flags = 0 ) {
+		++$this->setCount;
+		return true;
+	}
+
+	public function getterCount() {
+		return $this->getCount;
+	}
+
+	public function setterCount() {
+		return $this->setCount;
 	}
 }

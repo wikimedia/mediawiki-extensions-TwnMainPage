d73683139a0eae714a99d87b089548b0a5c6d14d
Niklas Laxström <niklas.laxstrom@gmail.com>
2023-09-27 16:23:23 +0300

    Add possibility to filter locations by category
    Example usage: {{#twn_get_locations:category=MediaWiki translators}}
    
    Bug: T346863
    Change-Id: Ic42a9925d55b80e0e3b1842e62970a08056bdbe4

--- src/Location/HookHandler.php
+++ src/Location/HookHandler.php
@@ -4,6 +4,8 @@ declare( strict_types = 1 );
 namespace Translatewiki\Location;
 
 use MediaWiki\Hook\ParserFirstCallInitHook;
+use MediaWiki\Title\MalformedTitleException;
+use MediaWiki\Title\TitleParser;
 use Parser;
 use Wikimedia\Rdbms\IConnectionProvider;
 
@@ -15,9 +17,14 @@ use Wikimedia\Rdbms\IConnectionProvider;
 class HookHandler implements ParserFirstCallInitHook {
 	private const PAGE_PROP_NAME = 'twncoords';
 	private IConnectionProvider $connectionProvider;
+	private TitleParser $titleParser;
 
-	public function __construct( IConnectionProvider $connectionProvider ) {
+	public function __construct(
+		IConnectionProvider $connectionProvider,
+		TitleParser $titleParser
+	) {
 		$this->connectionProvider = $connectionProvider;
+		$this->titleParser = $titleParser;
 	}
 
 	public function onParserFirstCallInit( $parser ) {
@@ -32,14 +39,45 @@ class HookHandler implements ParserFirstCallInitHook {
 		return '';
 	}
 
-	public function getLocations( Parser $parser ) {
-		$locations = $this->connectionProvider->getReplicaDatabase()->newSelectQueryBuilder()
+	public function getLocations( Parser $parser, ...$args ) {
+		$query = $this->connectionProvider->getReplicaDatabase()->newSelectQueryBuilder()
 			->select( 'pp_value' )
 			->from( 'page_props' )
-			->where( [ 'pp_propname' => self::PAGE_PROP_NAME ] )
-			->caller( __METHOD__ )
-			->fetchFieldValues();
+			->where( [ 'pp_propname' => self::PAGE_PROP_NAME ] );
 
+		$categoryDatabaseName = $this->getCategoryFilter( $args );
+		if ( $categoryDatabaseName !== null ) {
+			$query
+				->join( 'page', null, 'page_id=pp_page' )
+				->join( 'categorylinks', null, [ 'cl_from=page_id', 'cl_to' => $categoryDatabaseName ] );
+		}
+
+		$locations = $query->caller( __METHOD__ )->fetchFieldValues();
 		return implode( ';', $locations );
+	}
+
+	/** @param string[] $args */
+	private function getCategoryFilter( array $args ): ?string {
+		foreach ( $args as $arg ) {
+			if ( trim( $arg ) === '' ) {
+				continue;
+			}
+
+			[ $key, $value ] = array_map( 'trim', explode( '=', $arg, 2 ) );
+
+			if ( $key !== 'category' ) {
+				continue;
+			}
+
+			try {
+				return $this->titleParser->parseTitle( $value, NS_CATEGORY )->getDBkey();
+			} catch ( MalformedTitleException $e ) {
+				// If we just ignored invalid category param, we would not filter anything.
+				// By returning the original value, we add a query filter that will result
+				// in no rows found.
+				return $arg;
+			}
+		}
+		return null;
 	}
 }
